6f94eb39e0c340b6087357509f1d776e0f4b946d
Renê Xavier <renefx@gmail.com>
2021-09-20 16:15:38 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,26 @@
-
+/ swift-tools-version:5.1
 import PackageDescription
 
-let package = Package(name: "BeyovaJSON", dependencies : [], exclude: ["Tests"])
+let package = Package(
+    name: "BeyovaJSON",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "BeyovaJSON",
+            targets: ["BeyovaJSON"]),
+    ],
+    dependencies: [
+        // no dependencies
+    ],
+    targets: [
+        .target(
+            name: "BeyovaJSON iOS",
+            dependencies: []),
+        .target(
+            name: "BeyovaJSON macOS",
+            dependencies: ["BeyovaJSON"]),
+    ], 
+    exclude: ["Tests"]
+)
